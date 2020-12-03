c53e0489de41103da3fffef3970e2f91db755876
Minka <renalditriananda@gmail.com>
2020-12-03 07:39:46 +0700

    add loading 2

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,11 @@
+const loading = document.getElementById('loading-container')
+
 export function Start () {
   console.log('Loading starting')
-  const loading = document.getElementById('loading-container')
   loading.style.display = 'flex'
 }
 
 export function Stop () {
   console.log('Loading finished')
-  const loading = document.getElementById('loading-container')
   loading.style.display = 'none'
 }
